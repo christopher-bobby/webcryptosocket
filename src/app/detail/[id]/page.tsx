4f0e5bd5ed48506f7c22457205747af6d85bc751
Christopher <christopher@Christophers-MacBook-Pro.local>
2024-07-23 23:54:12 +0700

    chore: refactor some constants

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -4,9 +4,25 @@ import { NextPage } from 'next';
 import { useParams } from 'next/navigation';
 import TradingViewWidget from '../../../components/TradingViewWidget';
 import OrderBook from '../../../components/OrderBook';
-import { ASSETS } from '../../../constants';
+import { ASSETS , SYMBOLS} from '../../../constants';
 
 const symbolMap: { [key: string]: string } = {
+  btc: SYMBOLS.BTCUSD,
+  xrp: SYMBOLS.XRPUSD,
+  bnb: SYMBOLS.BNBUSD,
+  ada: SYMBOLS.ADAUSD,
+  sol: SYMBOLS.SOLUSD,
+  doge: SYMBOLS.DOGEUSD,
+  ava: SYMBOLS.AVAXUSD,
+  dot: SYMBOLS.DOTUSD,
+  uni: SYMBOLS.UNIUSD,
+  stellar: SYMBOLS.XLMUSD,
+  lite: SYMBOLS.LTCUSD,
+  link: SYMBOLS.LINKUSD,
+  vet: SYMBOLS.VETUSD,
+};
+
+const symbolOrderbook: { [key: string]: string } = {
   btc: ASSETS.BTCUSDT,
   xrp: ASSETS.XRPUSDT,
   bnb: ASSETS.BNBUSDT,
@@ -26,7 +42,7 @@ const symbolMap: { [key: string]: string } = {
 const DetailPage: NextPage = () => {
   const { id } = useParams<{ id: string }>();
   const symbol = symbolMap[id];
-  const symbolAssetOrderBook = symbolMap[id];
+  const symbolAssetOrderBook = symbolOrderbook[id];
   return (
     <div className='min-h-[100vh] bg-black'>
       <TradingViewWidget symbol={symbol}/>
